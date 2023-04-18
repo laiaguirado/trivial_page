63c30f95c28fc681253e775d6d93e9681b9cec65
Laia Guirado Campmany <laiaguirado1999@gmail.com>
2023-04-18 23:11:38 +0200

    add some notes

--- backend/server/src/trivial/trivial.service.js
+++ backend/server/src/trivial/trivial.service.js
@@ -13,4 +13,16 @@ const createTrivial = ({ numQuestion, question, difficulty, category, answers })
     return Trivial.create({ numQuestion, question, difficulty, category, answers });
 }
 
+//añadir get de una question de cada category
+
+//añadir get de una question de cada difficulty
+
+//añadir get de una question con una category y difficulty
+
+//añadir get de num questions de cada category
+
+//añadir get de num questions de cada difficulty
+
+
+
 module.exports = { getAllTrivial, getTrivial, createTrivial }
